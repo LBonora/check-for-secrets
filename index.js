7f254bf58fb847435020a7b14be624173868800d
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-12-30 09:18:25 +0000

    fix: general refactoring

--- index.js
+++ index.js
@@ -6,108 +6,17 @@ const exec = util.promisify(require("child_process").exec);
 const parameters = ["help", "git-cached"];
 
 async function checkForSecrets() {
-  var args = process.argv.slice(2);
+  let arguments = process.argv.slice(2);
 
-  if (args.length <= 0 || args[0] === null) {
-    console.log("No arguments were informed.\n");
-    console.log("Use the parameter `help` to check avaliable options\n");
-    process.exit(1);
-  }
+  ValidateArguments(arguments);
 
-  if (!parameters.includes(args[0])) {
-    console.log("Invalid argument.\n");
-    console.log("Use the parameter `help` to check avaliable options\n");
-    process.exit(1);
-  }
-
-  if (args[0].indexOf(parameters[0]) >= 0) {
-    process.stdout.write("\n\nUsage:\n");
-    process.stdout.write("help - show avaliable parameters\n");
-    process.stdout.write(
-      "git-cached - looks for the cached files and check for not allowed patterns\n"
-    );
-    process.stdout.write("\n\n");
-
-    process.exit(0);
-  }
-
-  let checkForSecretsConfig = null;
-
-  try {
-    const checkForSecretsConfig = require("../../checkforsecrets.config.json");
-  } catch (error) {
-    console.log(
-      "Using standard config. You can add your own config by creating a checkforsecrets.config.json file on your main directory\n"
-    );
-  }
-
-  if (checkForSecretsConfig === null)
-    checkForSecretsConfig = require("../check-for-secrets/checkforsecrets.config.json");
+  let checkForSecretsConfig = GetConfigParameters();
 
   const ignoredFiles = checkForSecretsConfig.ignoredFiles;
   const patterns = checkForSecretsConfig.patterns;
 
-  let notAllowedFiles = [];
-
-  if (args[0].indexOf(parameters[1]) >= 0) {
-    try {
-      let execReturn = await exec("git diff --cached --name-only");
-      if (execReturn.stdout !== null) {
-        let files = execReturn.stdout.split("\n");
-
-        if (files.length === 0) {
-          process.stdout.write("\n\n🟡 No git cached files found\n\n");
-          process.exit(0);
-        }
-
-        for (let index = 0; index < files.length; index++) {
-          const filePath = files[index];
-
-          if (filePath === "") continue;
-
-          if (ignoredFiles.includes(filePath)) {
-            console.log("ignored files: ", filePath);
-            continue;
-          }
-
-          const data = await fs.promises.readFile(filePath, "utf8");
-
-          for (let index = 0; index < patterns.length; index++) {
-            const regex = new RegExp(patterns[index], "i");
-            let regexResult = regex.exec(data);
-
-            if (regexResult != null && regexResult.length > 0) {
-              for (let index = 0; index < regexResult.length; index++) {
-                const patternFound = regexResult[index];
-                notAllowedFiles.push(
-                  filePath + " | string match: " + patternFound
-                );
-              }
-            }
-          }
-        }
-
-        return notAllowedFiles;
-      }
-    } catch (error) {
-      process.stdout.write("Error when running git-cached validation.\n");
-      if (process.stderr.find("usage: git diff [<options>]")) {
-        process.stdout.write(
-          "Invallid git diff cached command. Contact the creator for help.\n"
-        );
-        process.exit(1);
-      }
-
-      if (
-        process.stderr.find("git: command not found") ||
-        process.stderr.find(
-          "'git' is not recognized as an internal or external command"
-        )
-      ) {
-        process.stdout.write("Make sure git is installed in your system.\n");
-        process.exit(1);
-      }
-    }
+  if (arguments[0].indexOf(parameters[1]) >= 0) {
+    return checkForGitCachedFiles(ignoredFiles, patterns);
   }
 }
 
@@ -128,6 +37,113 @@ async function checkForSecrets() {
     process.exit(1);
   }
 
-  process.stdout.write("\n\n🟢 No bad patterns found\n\n");
+  process.stdout.write("\n\n🟢 No bad patterns found 🙂\n\n");
   process.exit(0);
 })();
+
+function ValidateArguments(arguments) {
+  if (arguments.length <= 0 || arguments[0] === null) {
+    console.log("No arguments were informed.\n");
+    console.log("Use the parameter `help` to check avaliable options\n");
+    process.exit(1);
+  }
+
+  if (!parameters.includes(arguments[0])) {
+    console.log("Invalid argument.\n");
+    console.log("Use the parameter `help` to check avaliable options\n");
+    process.exit(1);
+  }
+
+  if (arguments[0].indexOf(parameters[0]) >= 0) {
+    process.stdout.write("\n\nUsage:\n");
+    process.stdout.write("help - show avaliable parameters\n");
+    process.stdout.write(
+      "git-cached - looks for the cached files and check for not allowed patterns\n"
+    );
+    process.stdout.write("\n\n");
+
+    process.exit(0);
+  }
+}
+
+function GetConfigParameters() {
+  let checkForSecretsConfig = null;
+
+  try {
+    checkForSecretsConfig = require("../../checkforsecrets.config.json");
+  } catch (error) {
+    console.log(
+      "Using standard config. You can add your own config by creating a `checkforsecrets.config.json` file on your main directory.\n"
+    );
+  }
+
+  if (checkForSecretsConfig == null)
+    checkForSecretsConfig = require("../check-for-secrets/checkforsecrets.config.json");
+  return checkForSecretsConfig;
+}
+
+async function checkForGitCachedFiles(ignoredFiles, patterns) {
+  process.stdout.write("🟡 Looking for git cached files...\n");
+  try {
+    let notAllowedFiles = [];
+
+    let execReturn = await exec("git diff --cached --name-only");
+    if (execReturn.stdout !== null) {
+      let files = execReturn.stdout.split("\n");
+
+      if (files.length <= 1) {
+        process.stdout.write(
+          "\n\n🟡 No git cached files found. You must `git add` the changed files, so they can be checked. 🤔\n\n"
+        );
+        process.exit(0);
+      }
+
+      for (let index = 0; index < files.length; index++) {
+        const filePath = files[index];
+
+        if (filePath === "") continue;
+
+        if (ignoredFiles.includes(filePath)) {
+          console.log("ignored files: ", filePath);
+          continue;
+        }
+
+        const data = await fs.promises.readFile(filePath, "utf8");
+
+        for (let index = 0; index < patterns.length; index++) {
+          const regex = new RegExp(patterns[index], "i");
+          let regexResult = regex.exec(data);
+
+          if (regexResult != null && regexResult.length > 0) {
+            for (let index = 0; index < regexResult.length; index++) {
+              const patternFound = regexResult[index];
+              notAllowedFiles.push(
+                filePath + " | string match: " + patternFound
+              );
+            }
+          }
+        }
+      }
+
+      return notAllowedFiles;
+    }
+  } catch (error) {
+    process.stdout.write("Error when running git-cached validation.\n");
+    if (process.stderr.find("usage: git diff [<options>]")) {
+      process.stdout.write(
+        "Invallid git diff cached command. Contact the creator for help.\n"
+      );
+      process.exit(1);
+    }
+
+    if (
+      process.stderr.find("git: command not found") ||
+      process.stderr.find(
+        "'git' is not recognized as an internal or external command"
+      )
+    ) {
+      process.stdout.write("Make sure git is installed in your system.\n");
+      process.exit(1);
+    }
+  }
+}
